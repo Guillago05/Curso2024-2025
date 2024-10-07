76a0ee1362357acff9806d4d1ee85ec93f4ca34e
Guillago05 <164027310+Guillago05@users.noreply.github.com>
2024-10-07 23:31:21 +0200

    Update task07.py

--- Assignment4/course_materials/python/task07.py
+++ Assignment4/course_materials/python/task07.py
@@ -24,36 +24,94 @@ g.parse(github_storage+"/rdf/example6.rdf", format="xml")
 """**TASK 7.1: List all subclasses of "LivingThing" with RDFLib and SPARQL**"""
 
 # TO DO
+query = """
+    SELECT ?subclass WHERE {
+        ?subclass rdfs:subClassOf <http://somewhere#LivingThing> .
+    }
+"""
 # Visualize the results
-
-#for r in g.query(q1):
-#  print(r)
+results = g.query(query)
+for row in results:
+    print(row.subclass)
 
 """**TASK 7.2: List all individuals of "Person" with RDFLib and SPARQL (remember the subClasses)**
 
 """
 
 # TO DO
+query = """
+    SELECT ?individual WHERE {
+        ?individual a ?personClass .
+        ?personClass rdfs:subClassOf* <http://somewhere#Person> .
+    }
+"""
+results = g.query(query)
 # Visualize the results
 
+for row in results:
+    print(row.individual)
 """**TASK 7.3: List all individuals of just "Person" or "Animal". You do not need to list the individuals of the subclasses of person (in SPARQL only)**
 
 """
 
 # TO DO
-# Visualize the results
 
+query = """
+    SELECT ?individual WHERE {
+        { ?individual a <http://somewhere#Person> }
+        UNION
+        { ?individual a <http://somewhere#Animal> }
+    }
+"""
+results = g.query(query)
+
+# Visualize the results
+for row in results:
+    print(row.individual)
 """**TASK 7.4:  List the name of the persons who know Rocky (in SPARQL only)**"""
 
 # TO DO
+query = """
+    SELECT ?name WHERE {
+        ?person foaf:knows <http://somewhere#Rocky> .
+        ?person foaf:name ?name .
+    }
+"""
+results = g.query(query)
 # Visualize the results
+for row in results:
+    print(row.name)
 
 """**Task 7.5: List the name of those animals who know at least another animal in the graph (in SPARQL only)**"""
 
 # TO DO
-# Visualize the results
+query = """
+    SELECT DISTINCT ?animalName WHERE {
+        ?animal foaf:knows ?otherAnimal .
+        ?animal a <http://somewhere#Animal> .
+        ?otherAnimal a <http://somewhere#Animal> .
+        ?animal foaf:name ?animalName .
+    }
+"""
 
+results = g.query(query)
+# Visualize the results
+for row in results:
+    print(row.animalName)
 """**Task 7.6: List the age of all living things in descending order (in SPARQL only)**"""
 
 # TO DO
+query = """
+    SELECT ?livingThing ?age WHERE {
+        ?livingThing a <http://somewhere#LivingThing> .
+        ?livingThing <http://schema.org/age> ?age .
+    }
+    ORDER BY DESC(?age)
+"""
+
+results = g.query(query)
+
 # Visualize the results
+
+for row in results:
+    print(row.age)
